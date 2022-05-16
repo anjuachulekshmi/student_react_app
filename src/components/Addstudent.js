48422deacd096ae6344cc0dece15f8ab0250aa51
anjuachulekshmi <anjuachulekshmi836@gmail.com>
2022-05-17 04:33:36 +0530

    first commit

---
+++ src/components/Addstudent.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const Addstudent = () => {
+  return (
+    <div>
+        <div className="container">
+    <div className="row">
+        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <div className="row g-3">
+                <div className="col  col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Name</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div classNameName="col  col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">RollNo</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col  col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">Admn No</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col  col-6 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                    <label for="" className="form-label">University Reg No</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <label for="" className="form-label">college</label>
+                    <input type="text" className="form-control"/>
+                </div>
+                <div className="col  col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <button className="btn btn-success">REGISTER</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+    </div>
+  )
+}
+
+export default Addstudent
